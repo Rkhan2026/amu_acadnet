b1257e37cd9e3fd5ff0b2eee61860490e142406b
MRK <135884817+Rkhan2026@users.noreply.github.com>
2026-05-10 07:02:47 +0530

    removed comments

--- app/(public)/login/page.js
+++ app/(public)/login/page.js
@@ -8,10 +8,6 @@ export const metadata = {
 export default function LoginPage() {
   return (
     <div className="min-h-[calc(100vh-4rem)] flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-gray-50/50">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        {/* Logo is in Navbar, keeping this clean */}
-      </div>
-
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <LoginForm />
       </div>
--- app/(public)/register/page.js
+++ app/(public)/register/page.js
@@ -8,10 +8,6 @@ export const metadata = {
 export default function RegisterPage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8 bg-[url('/grid-pattern.svg')]">
-      <div className="sm:mx-auto sm:w-full sm:max-w-md mb-8">
-        {/* Logo removed as it is in Global Navbar */}
-      </div>
-
       <div className="sm:mx-auto sm:w-full sm:max-w-125">
         <RegisterForm />
       </div>
